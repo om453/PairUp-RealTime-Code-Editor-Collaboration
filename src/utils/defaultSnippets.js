3cb6e6194cdcdd0beff45f41349ad96ef9bd2652
Om Patel <om.patel200212@gmail.com>
2024-12-29 22:49:22 +0530

    Refactor default snippets: Remove unnecessary line breaks for cleaner code

--- src/utils/defaultSnippets.js
+++ src/utils/defaultSnippets.js
@@ -1,12 +1,9 @@
 export const defaultSnippets = {
-    javascript: `
-console.log("Hello, World!");`,
+    javascript: `console.log("Hello, World!");`,
 
-    python: `
-print("Hello, World!")`,
+    python: `print("Hello, World!")`,
 
-    cpp: `
-#include <iostream>
+    cpp: `#include <iostream>
 using namespace std;
 
 int main() {
@@ -14,8 +11,7 @@ int main() {
     return 0;
 }`,
 
-    java: `
-public class Main {
+    java: `public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 }`
